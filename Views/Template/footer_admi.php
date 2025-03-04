b13d6839ab4bd8a2510a57c26703058e7ebc089c
Edwin Ivan Moysen Rodriguez <edwin_mr199@outlook.com>
2025-03-04 00:38:32 -0600

    Se modifica el archivo roles.php al cual se le agrega una tabla, que contendra los roles de los usuarios en una base de datos

--- Views/Template/footer_admi.php
+++ Views/Template/footer_admi.php
@@ -7,6 +7,9 @@
     <!-- Importacion del archivo functions_admi.js para las funciones del administrador-->
     <script src="<?php echo media(); ?>/js/functions_admi.js"></script>
     <!-- Page specific javascripts-->
+    <!-- Data table plugin-->
+    <script type="text/javascript" src="<?php echo media(); ?>/js/plugins/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="<?php echo media(); ?>/js/plugins/dataTables.bootstrap.min.js"></script>
     <!-- Importacion del archivo functions_roles.js para las funciones del administrador-->
     <script src="<?php echo media(); ?>/js/functions_roles.js"></script>
   </body>
