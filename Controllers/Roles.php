fabd2758023b27b6a53902e3fea7d1a23195ba62
Edwin Ivan Moysen Rodriguez <edwin_mr199@outlook.com>
2025-03-03 20:25:53 -0600

    Creacion del controlador Roles, el cual contendran los roles de usuario

---
+++ Controllers/Roles.php
@@ -0,0 +1,24 @@
+<?php  
+	/**
+	  * Archivo Roles.php
+	  * Controlador Dashboard
+  	*/
+	class Roles extends Controllers
+	{
+		//Constructor
+		public function __construct()
+		{
+			parent::__construct();
+		}
+		//Metodo: llamada a la vista roles
+		public function roles($params)
+		{
+			//Arreglo qu contiene toda la informacion de nuestra vista
+			$data['page_id'] = 3;
+			$data['page_tag'] = "Roles Usuario";
+			$data['page_title'] = "Roles Usuario <small>Tenda Viertual</small>";
+			$data['page_name'] = "rol_usuario";
+			$this->views->getViews($this, "roles", $data);
+		}
+	}
+?>
--- Views/Template/footer_admi.php
+++ Views/Template/footer_admi.php
@@ -2,6 +2,9 @@
     <script src="<?php echo media(); ?>/js/jquery-3.7.0.min.js"></script>
     <script src="<?php echo media(); ?>/js/bootstrap.min.js"></script>
     <script src="<?php echo media(); ?>/js/main.js"></script>
+    <!-- Importacion del archivo fontawesome.js el cual contiene nuestra librearia de icons -->
+    <script src="<?php echo media(); ?>/js/fontawesome.js"></script>
+    <!-- Importacion del archivo functions_admi.js para las funciones del administrador-->
     <script src="<?php echo media(); ?>/js/functions_admi.js"></script>
     <!-- Page specific javascripts-->
     
