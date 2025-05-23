0018b29483944abbbc0bc4556530385c85d2325d
Edwin Ivan Moysen Rodriguez <edwin_mr199@outlook.com>
2025-05-23 02:41:37 -0600

    Se crea la vista de usuarios.

---
+++ Controllers/Usuarios.php
@@ -0,0 +1,23 @@
+<?php 
+	/**
+	  * Archivo Usuarios.php
+	  * Controlador Usuarios
+  	*/
+	class Usuarios extends Controllers
+	{
+		//Constructor
+		public function __construct()
+		{
+			parent::__construct();
+		}
+		//Metodo: llamada a la vista home
+		public function home($params)
+		{
+			//Arreglo qu contiene toda la informacion de nuestra vista
+			$data['page_tag'] = "Usuarios";
+			$data['page_title'] = "Usuarios <small>Tenda Viertual</small>";
+			$data['page_name'] = "usuarios";
+			$this->views->getViews($this, "usuarios", $data);
+		}
+	}
+ ?>
---
+++ Views/Usuarios/usuarios.php
@@ -0,0 +1,45 @@
+<?php 
+  headerAdmi($data); 
+  //getModal("modalRoles", $data)
+?>
+    <main class="app-content">
+      <div class="app-title">
+        <div>
+          <h1><i class="app-menu__iconbi bi-check2-square"> </i><?php echo $data['page_title'] ?>
+            <button class="btn btn-primary" type="button" onclick="openModal();"><i class="bi bi-plus"></i> Nuevo</button>
+          </h1>
+        </div>
+        <ul class="app-breadcrumb breadcrumb">
+          <li class="breadcrumb-item"><i class="bi bi-house-door fs-6"></i></li>
+          <li class="breadcrumb-item"><a href="<?php base_url(); ?>"><?php echo $data['page_title'] ?></a></li>
+        </ul>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div class="tile">
+            <div class="tile-body">
+              <div class="table-responsive">
+                <table class="table table-hover table-bordered" id="tableRoles">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Nombres</th>
+                      <th>Apellidos</th>
+                      <th>Email</th>
+                      <th>Teléfono</th>
+                      <th>Rol</th>
+                      <th>Status</th>
+                      <th>Acciones</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+<?php footerAdmi($data); ?>
