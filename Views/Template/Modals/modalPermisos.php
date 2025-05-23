77e661fde33b0c80431a16cd9d95b4e96b5aa0c8
Edwin Ivan Moysen Rodriguez <edwin_mr199@outlook.com>
2025-05-23 02:23:40 -0600

    Se modifican nombre en modals y se corrige funciones en botones del modal roles y permisos roles.

--- Views/Template/Modals/modalPermisos.php
+++ Views/Template/Modals/modalPermisos.php
@@ -4,9 +4,9 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title h4">Permisos Roles de Usuraios</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">X</span>
-        </button>
+       <button type="button" class="close" onclick="$('#modalPermisos').modal('hide')" aria-label="Close">
+        <span aria-hidden="true">×</span>
+      </button>
       </div>
       <!--Contenido del modal-->
       <div class="modal-body">
@@ -20,8 +20,8 @@
                     <tr>
                       <th>#</th>
                       <th>Módulo</th>
-                      <th class="text-center">Leer</th>
-                      <th class="text-center">Escribir</th>
+                      <th class="text-center">Ver</th>
+                      <th class="text-center">Crear</th>
                       <th class="text-center">Actualizar</th>
                       <th class="text-center">Eliminar</th>
                     </tr>
@@ -69,7 +69,8 @@
               </div>
               <div class="text-center">
                 <button class="btn btn-success" type="button" onclick="fntSavePermisos(event)">Guardar</button>
-                <button class="btn btn-danger" type="button">Cancelar</button>
+                <button class="btn btn-danger" type="button" onclick="$('#modalPermisos').modal('hide')">Cancelar</button>
+
               </div>
             </form>
           </div>
